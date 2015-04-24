e59227c6bf36b6c72dab5bba2cace56612e611a7
David Kohreidze <david@fountain.com>
2015-04-23 23:32:19 -0700

    formatting

--- about-data.py
+++ about-data.py
@@ -25,43 +25,43 @@ with open('inputURLs.csv', 'rU') as cf:
 		l = l + row			
 
 for current in l:
-		# retrieves the resources from webpage
-		r = requests.get(current)
+	# retrieves the resources from webpage
+	r = requests.get(current)
 
-		# parses HTML document
-		soup = BeautifulSoup(r.text)
+	# parses HTML document
+	soup = BeautifulSoup(r.text)
 
-		# print the webpage we're scraping to output file
-		f.write(current+',')
-		print(current)
+	# print the webpage we're scraping to output file
+	f.write(current+',')
+	print(current)
 
-		# scrapes the subscriber and view count
-		b = soup.find_all("li", "about-stat ") #trailing space is required.
-		for i in b:
-			value = i.b.text.strip().replace(',','')
-			name = i.b.next_sibling.strip().replace(',','')
-			f.write(value+',')
-			print('%s = %s') % (name, value)
+	# scrapes the subscriber and view count
+	b = soup.find_all("li", "about-stat ") #trailing space is required.
+	for i in b:
+		value = i.b.text.strip().replace(',','')
+		name = i.b.next_sibling.strip().replace(',','')
+		f.write(value+',')
+		print('%s = %s') % (name, value)
 
-		# scrapes the description
-		c = soup.find_all("div", "about-description")
-		for i in c:
-			description = i.text.strip().replace('\n',' ').replace(',','').encode("utf-8")
-			f.write(description+',')
-			print(description)
+	# scrapes the description
+	c = soup.find_all("div", "about-description")
+	for i in c:
+		description = i.text.strip().replace('\n',' ').replace(',','').encode("utf-8")
+		f.write(description+',')
+		print(description)
 
-		# scrapes all the external links 
-		a = soup.find_all("a", "about-channel-link ") #trailing space is required.
-		for i in a:
-			title = i.get('title').strip().replace(',','').encode("utf-8")
-			url = i.get('href')
-			f.write(title+','+url+',')
-			print(url)
+	# scrapes all the external links 
+	a = soup.find_all("a", "about-channel-link ") #trailing space is required.
+	for i in a:
+		title = i.get('title').strip().replace(',','').encode("utf-8")
+		url = i.get('href')
+		f.write(title+','+url+',')
+		print(url)
 
-		f.write('\n')	
-		print('\n')
+	f.write('\n')	
+	print('\n')
 
-		time.sleep(3)
+	time.sleep(3)
 
 cf.close()
 f.close()
